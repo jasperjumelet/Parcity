bd4995e962c6e7966af218d1e35f591891533f6c
Jasper <jasperjumelet@hotmail.com>
2022-11-24 23:53:36 +0100

    fixed clickable nav

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -5,7 +5,7 @@ const Home = () => {
     <main>
       <div className="relative bg-gradient-to-b from-primary to-secondary/70 min-h-screen">
         <NavBar />
-        <div className="absolute flex flex-col min-h-screen justify-end">
+        <div className="flex flex-col min-h-screen justify-end">
           <img
             src={"/images/city2.png"}
             className="flex bottom-0 align-bottom opacity-70 saturate-150"
