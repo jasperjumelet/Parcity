7be5b9134cef2cd26c1fc3436c163115a5978b33
Jasper <jasperjumelet@hotmail.com>
2022-12-17 19:49:44 +0100

    added admin selection page

--- client/src/components/AdminPages/Admin.tsx
+++ client/src/components/AdminPages/Admin.tsx
@@ -6,42 +6,16 @@ const Admin = () => {
     <div className="bg-gradient-to-b from-primary to-secondary/70 min-h-screen">
       <NavBar />
       <div className="p-12"></div> {/* use this to add a invisible block at navbar */}
-      <div className="grid grid-cols-2 gap-10">
-        <div className="mx-10 h-screen bg-gray-900">
-          <div className="flex flex-col">
-            <div className="flex flex-row justify-center">
-              <h2 className="font-jetbrains-mono justify-center font-[600] text-4xl text-gray-100">Manage Users</h2>
-            </div>
-            <div className="grid grid-cols-2 grid-rows-2 gap-9 mt-8">
-              {/* Need to add more content here */}
-              <div className="grid-col-1 grid-row-1 h-80 bg-green-400">
-              </div>
-              <div className="grid-col-2 grid-row-1 h-80 bg-green-400">
-              </div>
-              <div className="grid-col-1 grid-row-2 h-80 bg-green-400">
-              </div>
-              <div className="grid-col-2 grid-row-2 h-80 bg-green-400">
-              </div>
-            </div>
+      <div className="grid grid-cols-2">
+        <div className="h-96 border-8 border-blue-600 bg-gray-100 mx-10 mt-48 rounded-3xl flex flex-col justify-center text-black hover:border-orange-600 hover:bg-black hover:text-gray-100">
+          <div className="flex justify-center text-center">
+            <h3 className="font-jetbrains-mono font-[500] text-xl md:text-5xl">Manage Users</h3>
           </div>
-
         </div>
-        <div className="mx-10 h-screen bg-gray-900">
-          <div className="flex flex-col ">
-            <div className="flex flex-row justify-center">
-              <h2 className="font-jetbrains-mono font-[600] text-4xl text-gray-100">Manage Properties</h2>
-            </div>
-            <div className="grid grid-cols-2 grid-rows-2 gap-9 mt-8">
-              {/* Need to add more content here */}
-              <div className="grid-col-1 grid-row-1 h-80 bg-green-400">
-              </div>
-              <div className="grid-col-2 grid-row-1 h-80 bg-green-400">
-              </div>
-              <div className="grid-col-1 grid-row-2 h-80 bg-green-400">
-              </div>
-              <div className="grid-col-2 grid-row-2 h-80 bg-green-400">
-              </div>
-            </div>
+
+        <div className="h-96 border-8 border-blue-600 bg-gray-100 mx-10 mt-48 rounded-3xl flex flex-col justify-center text-black hover:border-orange-600 hover:bg-black hover:text-gray-100">
+          <div className="flex justify-center text-center">
+            <h3 className="font-jetbrains-mono font-[500] text-xl md:text-5xl">Manage Properties</h3>
           </div>
         </div>
 
